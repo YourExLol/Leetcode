e88cfc1bceafed154d692970db5bde51ab4a0b79
YourExLol <71457329+YourExLol@users.noreply.github.com>
2024-11-15 15:05:16 +0530

    Create 3. Longest Substring Without Repeating Characters.cpp

---
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,34 @@
+## Problem Statement ##
+## 3. Longest Substring Without Repeating Characters
+## https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+############################################### Code begins here ##############################################################
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        set<char> st; //to check which element have apperaed
+        int g_ans=0;  //return ans
+
+        //2 pointer algo
+        int i=0; //left
+        int j=0; //eight
+
+        int size=s.size();
+
+        while(j<size){
+            if(st.count(s[j])==0){
+                st.insert(s[j]);            // if not found Insert character into set
+                g_ans = max(g_ans, j - i + 1); // Update the global max length
+                 j++;  //expand the window on right side bu 1 unit
+            }
+            else {
+                st.erase(s[i]);
+                i++;
+            }
+        } //while ends here
+
+        return g_ans;
+    }
+};
