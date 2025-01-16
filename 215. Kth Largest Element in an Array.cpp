00b0bbaf9932ea084158f79984e9c687c8190d28
YourExLol <71457329+YourExLol@users.noreply.github.com>
2025-01-16 18:42:01 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -4,12 +4,14 @@
 
 ############################################### Code begins here ##############################################################
 
+//Priority Queue
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> pq(nums.begin(), nums.begin() + k); //create min heap of K elements
+        priority_queue<int,vector<int>,greater<int>> pq(nums.begin(), nums.begin() + k);
         for(int i=k;i<nums.size();i++){
-            if(nums[i]>pq.top()){      //currently pq.top() is k the largest element but if there exist a larger element than it, it becomes k+1th largest element. so we pop out. So pq.top() will always have kth largest element
+            if(nums[i]>pq.top()){
                 pq.pop();
                 pq.push(nums[i]);
             }
@@ -17,3 +19,39 @@ public:
         return pq.top();
     }
 };
+
+//Counting Sort
+
+int kthSmallest(int arr[], int n, int k) {
+    // First, find the maximum element in the array
+    int max_element = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_element) {
+            max_element = arr[i];
+        }
+    }
+
+    // Create an array to store the frequency of each 
+   // element in the input array
+    int freq[max_element + 1] = {0};
+    for (int i = 0; i < n; i++) {
+        freq[arr[i]]++;
+    }
+
+    // Keep track of the cumulative frequency of elements 
+   // in the input array
+    int count = 0;
+    for (int i = 0; i <= max_element; i++) {
+        if (freq[i] != 0) {
+            count += freq[i];
+            if (count >= k) {
+                // If we have seen k or more elements, 
+              // return the current element
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+// Quick Sort
